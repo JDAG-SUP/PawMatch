171b0f419c7850822ea7a5d79d5220c998d26320
JDAG-SUP <juan.acevedog@upb.edu.co>
2026-04-19 18:34:13 -0500

    build: generate build artifacts and temporary files for Android and shared modules

--- shared/src/commonMain/kotlin/com/pawmatch/app/shared/presentation/viewmodels/BaseViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.pawmatch.app.shared.presentation.viewmodels
-
-import kotlinx.coroutines.CoroutineScope
-
-expect abstract class BaseViewModel() {
-    val viewModelScope: CoroutineScope
-    protected open fun onCleared()
-}
